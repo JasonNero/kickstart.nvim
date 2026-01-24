88ab133c632def8eb136f68a257da798250a5d97
Jason Schuehlein <jason@schuehlein.eu>
2026-01-24 15:51:07 +0100

    add PlatformIO

--- init.lua
+++ init.lua
@@ -1073,7 +1073,58 @@ require('lazy').setup({
       -- restriction_mode = "hint", -- Default is "block"
     },
   },
+  {
+    'anurag3301/nvim-platformio.lua',
+    -- cmd = { 'Pioinit', 'Piorun', 'Piocmdh', 'Piocmdf', 'Piolib', 'Piomon', 'Piodebug', 'Piodb' },
 
+    -- optional: cond used to enable/disable platformio
+    -- based on existance of platformio.ini file and .pio folder in cwd.
+    -- You can enable platformio plugin, using :Pioinit command
+    cond = function()
+      -- local platformioRootDir = vim.fs.root(vim.fn.getcwd(), { 'platformio.ini' }) -- cwd and parents
+      local platformioRootDir = (vim.fn.filereadable 'platformio.ini' == 1) and vim.fn.getcwd() or nil
+      if platformioRootDir and vim.fs.find('.pio', { path = platformioRootDir, type = 'directory' })[1] then
+        -- if platformio.ini file and .pio folder exist in cwd, enable plugin to install plugin (if not istalled) and load it.
+        vim.g.platformioRootDir = platformioRootDir
+      elseif (vim.uv or vim.loop).fs_stat(vim.fn.stdpath 'data' .. '/lazy/nvim-platformio.lua') == nil then
+        -- if nvim-platformio not installed, enable plugin to install it first time
+        vim.g.platformioRootDir = vim.fn.getcwd()
+      else -- if nvim-platformio.lua installed but disabled, create Pioinit command
+        vim.api.nvim_create_user_command('Pioinit', function() --available only if no platformio.ini and .pio in cwd
+          vim.api.nvim_create_autocmd('User', {
+            pattern = { 'LazyRestore', 'LazyLoad' },
+            once = true,
+            callback = function(args)
+              if args.match == 'LazyRestore' then
+                require('lazy').load { plugins = { 'nvim-platformio.lua' } }
+              elseif args.match == 'LazyLoad' then
+                local pio_install_status = require('platformio.utils').pio_install_check()
+                if not pio_install_status then
+                  return
+                end
+                vim.notify('PlatformIO loaded', vim.log.levels.INFO, { title = 'PlatformIO' })
+                require('platformio').setup(vim.g.pioConfig)
+                vim.cmd 'Pioinit'
+              end
+            end,
+          })
+          vim.g.platformioRootDir = vim.fn.getcwd()
+          require('lazy').restore { plguins = { 'nvim-platformio.lua' }, show = false }
+        end, {})
+      end
+      return vim.g.platformioRootDir ~= nil
+    end,
+
+    -- Dependencies are lazy-loaded by default unless specified otherwise.
+    dependencies = {
+      { 'akinsho/toggleterm.nvim' },
+      { 'nvim-telescope/telescope.nvim' },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'nvim-lua/plenary.nvim' },
+      { 'folke/which-key.nvim' },
+      { 'nvim-treesitter/nvim-treesitter' },
+    },
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
@@ -1121,6 +1172,18 @@ require('lazy').setup({
     },
   },
 })
+
+-- PlatformIO configuration
+vim.g.pioConfig = {
+  lsp = 'clangd',
+  clangd_source = 'ccls',
+  menu_key = '<leader>\\',
+  debug = false,
+}
+local pok, platformio = pcall(require, 'platformio')
+if pok then
+  platformio.setup(vim.g.pioConfig)
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
